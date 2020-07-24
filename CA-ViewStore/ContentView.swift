40ebf42fa4f5606f444c23d7dac977a9fd8ac544
George Kaimakas <gkaimakas@gmail.com>
2020-07-24 11:06:53 +0300

    Initial Commit

---
+++ CA-ViewStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CA-ViewStore
+//
+//  Created by George Kaimakas on 24/7/20.
+//  Copyright © 2020 gkaimakas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
